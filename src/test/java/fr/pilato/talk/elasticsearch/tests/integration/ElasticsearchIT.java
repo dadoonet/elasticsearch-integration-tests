b674bdeacd2ad3aec7f2b208ad3fa9d1bb009b6a
David Pilato <david@pilato.fr>
2018-03-20 16:22:52 +0100

    Add the first test

---
+++ src/test/java/fr/pilato/talk/elasticsearch/tests/integration/ElasticsearchIT.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package fr.pilato.talk.elasticsearch.tests.integration;
+
+import org.apache.http.HttpHost;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.main.MainResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ElasticsearchIT {
+
+    private static final Logger logger = LogManager.getLogger(ElasticsearchIT.class);
+    private static RestHighLevelClient client;
+    private static final String INDEX = "scenario1";
+
+    @BeforeClass
+    public static void startElasticsearchRestClient() throws IOException {
+        int testClusterPort = Integer.parseInt(System.getProperty("tests.cluster.port", "9200"));
+        String testClusterHost = System.getProperty("tests.cluster.host", "localhost");
+        String testClusterScheme = System.getProperty("tests.cluster.scheme", "http");
+
+        logger.info("Starting a client on {}://{}:{}", testClusterScheme, testClusterHost, testClusterPort);
+
+        // We start a client
+        RestClientBuilder builder = getClientBuilder(new HttpHost(testClusterHost, testClusterPort, testClusterScheme));
+        client = new RestHighLevelClient(builder);
+
+        // We make sure the cluster is running
+        MainResponse info = client.info();
+        logger.info("Client is running against an elasticsearch cluster {}.", info.getVersion().toString());
+    }
+
+    @AfterClass
+    public static void stopElasticsearchRestClient() throws IOException {
+        if (client != null) {
+            logger.info("Closing elasticsearch client.");
+            client.close();
+        }
+    }
+
+    private static RestClientBuilder getClientBuilder(HttpHost host) {
+        return RestClient.builder(host);
+    }
+
+    @Test
+    public void testAScenario() throws IOException {
+        // We remove any existing index
+        try {
+            logger.info("-> Removing index {}.", INDEX);
+            client.indices().delete(new DeleteIndexRequest(INDEX));
+        } catch (ElasticsearchStatusException e) {
+            assertThat(e.status().getStatus(), is(404));
+        }
+
+        // We create a new index
+        logger.info("-> Creating index {}.", INDEX);
+        client.indices().create(new CreateIndexRequest(INDEX));
+
+        // We index some documents
+        logger.info("-> Indexing one document in {}.", INDEX);
+        IndexResponse ir = client.index(new IndexRequest(INDEX, "doc").source(
+                jsonBuilder()
+                        .startObject()
+                        .field("foo", "bar")
+                        .endObject()
+        ).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE));
+        logger.info("-> Document indexed with _id {}.", ir.getId());
+
+        // We search
+        SearchResponse sr = client.search(new SearchRequest(INDEX));
+        logger.info("{}", sr);
+        assertThat(sr.getHits().totalHits, is(1L));
+    }
+}
